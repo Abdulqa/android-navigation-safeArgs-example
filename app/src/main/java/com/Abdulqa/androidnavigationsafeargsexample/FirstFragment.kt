9baeeb62b8347a9a337fce94279a4883e901753f
DESKTOP-KQBFKSC\abdul <AbdulQadir>
2022-12-27 17:57:07 +0500

    passing action to navigation

--- app/src/main/java/com/Abdulqa/androidnavigationsafeargsexample/FirstFragment.kt
+++ app/src/main/java/com/Abdulqa/androidnavigationsafeargsexample/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.Abdulqa.androidnavigationsafeargsexample
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.Abdulqa.androidnavigationsafeargsexample.databinding.FragmentFirstBinding
 
@@ -33,8 +33,9 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            FirstFragmentDirections.actionFirstFragmentToSecondFragment("Hello from SafeArgs!")
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val action =
+                FirstFragmentDirections.actionFirstFragmentToSecondFragment("Hello from SafeArgs!")
+            findNavController().navigate(action)
         }
     }
 
